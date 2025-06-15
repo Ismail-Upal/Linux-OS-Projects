76bf55a76171d404ea0ce0815d35efa14cd703b0
Ismail Maowla Upal <146773081+Ismail-Upal@users.noreply.github.com>
2025-06-15 21:10:00 +0600

    Update gen.cpp

--- stress-testing/gen.cpp
+++ stress-testing/gen.cpp
@@ -1,17 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int rnd(int l, int r){
-    return l + rand() % (r - l + 1);
+mt19937 rng;
+int random(int a, int b) {
+    return uniform_int_distribution<int>(a, b)(rng);
 }
 
 int main(int argc, char* argv[]) {
-    srand(atoi(argv[1]));
+    rng.seed(argc > 1 ? atoi(argv[1]) : chrono::steady_clock::now().time_since_epoch().count());
+    
+    int n = random(1, 100);
+    cout << n << endl;
 
-    int n = rnd(1, 100000);
-    int k = rnd(1, 100000);
-
-    cout << n << " " << k;
+    for(int i = 1; i <= n; i++){
+        int x = random(-100, 100);
+        cout << x << ' ';
+    }
+    
     cout << endl;
 
     return 0;
