476d936088269b7508c91fda3ddcca60d2d73dc1
Ismail Maowla Upal <146773081+Ismail-Upal@users.noreply.github.com>
2025-06-15 21:08:44 +0600

    Update brute.cpp

--- stress-testing/brute.cpp
+++ stress-testing/brute.cpp
@@ -1,35 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define opt() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-#define tc int t; cin >> t; for (int _ = 1; _ <= t; _++)
-using ll = long long;
-using i128 = __int128_t;
-#define endl '\n'
-#define sz(x) (ll)(x).size()
-//-------------------------------------------
 
-int32_t main()
+int main()
 {   
-    opt();
-    
-    ll mod = 1e9;
-    int n, k; cin >> n >> k;
-    vector<ll> v(n + 1);
+    int n; cin >> n;
 
-    ll sum = k;
-    for(int i = 0; i < min(n + 1, k); i++) v[i] = 1;
+    int x[n];
 
-    int l = 0, r = k;
-    while(r <= n){
-        v[r] = sum;
-        sum = (sum - v[l] + mod) % mod;
-        sum += v[r];
-        sum %= (ll) 1e9;
-        l++, r++;
+    for(int i = 0; i < n; i++){
+        cin >> x[i];
     }
-    
-    cout << v[n];
-    cout << endl;
 
+    int ans = 0;
+
+    for(int i = 0; i < n; i++){
+        int sum = 0;
+        for(int j = i; j < n; j++){
+            sum += x[j];
+            if(sum == 0) ans++;
+        }
+    }
+
+    cout << ans << endl;
+    
     return 0;
 }
