805f311a44b1ff12e6d923d8f7168186a4580ce4
Ismail Maowla Upal <146773081+Ismail-Upal@users.noreply.github.com>
2025-06-15 21:08:25 +0600

    Update a.cpp

--- stress-testing/a.cpp
+++ stress-testing/a.cpp
@@ -1,36 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define opt() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-#define tc int t; cin >> t; for (int _ = 1; _ <= t; _++)
-using ll = long long;
-using i128 = __int128_t;
-#define endl '\n'
-#define sz(x) (ll)(x).size()
-//-------------------------------------------
 
-int32_t main()
+int main()
 {   
-    opt();
+    int n; cin >> n;
+    int x[n];
     
-    int n, k; cin >> n >> k;
-    vector<ll> v(n + 1);
+    int ans = 0;
 
-    ll sum = k;
-    for(int i = 0; i < min(n + 1, k); i++) v[i] = 1;
+    int prefix_sum = 0;
 
-    int l = 0, r = k;
-    while(r <= n){
-        v[r] = sum;
-        sum -= v[l];
-        sum += v[r];
-        sum %= (ll) 1e9;
-        l++, r++;
+    map<int, int> fr;
+
+    for(int i = 0; i < n; i++){
+        cin >> x[i];
+
+        prefix_sum += x[i];
+
+        ans += fr[prefix_sum];
+
+        fr[prefix_sum]++;
     }
-    
-    cout << v[n] << endl;
 
+    cout << ans << endl;
+    
     return 0;
 }
-
-
-// https://atcoder.jp/contests/abc401/tasks/abc401_c
